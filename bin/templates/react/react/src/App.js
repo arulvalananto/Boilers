54acc1b97da28121cd7eafeebf050c1482cecb85
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-08-27 20:06:41 +0530

    add: REACT template

---
+++ bin/templates/react/react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+import './global.css'
+
+const App = () => {
+  return <h1>Hello, Welcome to React Boilerplate</h1>
+}
+
+export default hot(module)(App)
